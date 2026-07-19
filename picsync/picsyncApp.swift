c1e39ca7d7c0a41db897f8c5e60274e5fa643ab1
kevin.krausse <kkrausse97@gmail.com>
2026-07-19 08:33:25 -0700

    Initial Commit

---
+++ picsync/picsyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  picsyncApp.swift
+//  picsync
+//
+//  Created by kevin krausse on 7/19/26.
+//
+
+import SwiftUI
+
+@main
+struct picsyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ picsyncTests/picsyncTests.swift
@@ -0,0 +1,17 @@
+//
+//  picsyncTests.swift
+//  picsyncTests
+//
+//  Created by kevin krausse on 7/19/26.
+//
+
+import Testing
+@testable import picsync
+
+struct picsyncTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
