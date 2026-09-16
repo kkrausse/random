466c324fab8656ca67a59252b12576c72c4002de
kevin.krausse <kkrausse97@gmail.com>
2026-09-15 17:17:18 -0700

    Dock todo demo editor beside the preview in a responsive sidebar

--- browser-container-poc/todo-app-demo/src/editor-panel.tsx
+++ browser-container-poc/todo-app-demo/src/editor-panel.tsx
@@ -1,10 +1,13 @@
 import { PreparedBrowserEditor } from '@kev-browser-agent-kit/opencode-chat/editor'
 import '@kev-browser-agent-kit/opencode-chat/editor.css'
+import './editor-panel.css'
 
 const hostPaths = ['/api']
 const isPreviewReady = (frame: HTMLIFrameElement) => !!frame.contentDocument?.querySelector('main input#title:not(:disabled)')
 
 export default function EditorPanel({ onExit }: { onExit(): void }) {
-  return <PreparedBrowserEditor hostPaths={hostPaths}
-    initialPath="/src/home.tsx" isPreviewReady={isPreviewReady} onExit={onExit} />
+  return <div className="todo-editor-sidebar">
+    <PreparedBrowserEditor hostPaths={hostPaths}
+      initialPath="/src/home.tsx" isPreviewReady={isPreviewReady} onExit={onExit} />
+  </div>
 }
